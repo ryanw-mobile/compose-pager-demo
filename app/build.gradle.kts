7b919535edce1726127c4bd59f82720ebae188d5
Ryan Wong <ryan@rwmobi.com>
2023-08-12 18:02:04 +0100

    [chore] App: elevate compile and target sdk from 33 to 34.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.rwmobi.composepager"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.rwmobi.composepager"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
