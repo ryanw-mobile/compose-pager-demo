0e80222fd02cd30ba025e583ead410f081e9e62a
Ryan W <ryan@rwmobi.com>
2024-08-01 21:44:52 +0100

    [chore] App; bump version to 1.2.0 (build 3)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,8 +51,8 @@ android {
         applicationId = "com.rwmobi.composepager"
         minSdk = 24
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
