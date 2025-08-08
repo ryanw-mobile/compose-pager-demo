56d562f472709b0de967f9e7850e3b6f7078ea6a
Ryan W <ryan@rwmobi.com>
2025-08-08 23:52:57 +0100

    [chore] Gradle: apply latest template updates

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.com.android.application) apply false
     alias(libs.plugins.org.jetbrains.kotlin.android) apply false
     alias(libs.plugins.compose.compiler) apply false
-
+    alias(libs.plugins.detekt) apply false
+    alias(libs.plugins.kotlinter) apply false
 }
